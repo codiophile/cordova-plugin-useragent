f229c68518d97722cf19571f61d11e6d88c043fd
Erik Blomqvist <Erik.Blomqvist@Gamesys.co.uk>
2015-09-02 11:52:55 +0100

    Implemented Android part.
    Did not test it yet, so it may not work.

--- src/android/UserAgent.java
+++ src/android/UserAgent.java
@@ -6,13 +6,31 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class UserAgent extends CordovaPlugin {
+    private static final String kUserAgent = "useragent";
+    private static final String kUserAgentPrefix = "useragentprefix";
+    private static final String kUserAgentPostfix = "useragentpostfix";
+
     @Override
-	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-	    super.initialize(cordova, webView);
-        // TODO: Load cordova settings
-        String userAgent = null;
-        // TODO: Set user agent
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        // Load cordova settings
+        String userAgent = cordova.getActivity().getIntent().getStringExtra(kUserAgent);
+        String userAgentPrefix = cordova.getActivity().getIntent().getStringExtra(kUserAgentPrefix);
+        String userAgentPostfix = cordova.getActivity().getIntent().getStringExtra(kUserAgentPostfix);
+
+        // Build user agent
         WebSettings settings = ((WebView) webView.getEngine().getView()).getSettings();
-        settings.setUserAgentString();
-	}
+        if (userAgent == null) {
+            userAgent = settings.getUserAgentString();
+        }
+        if (userAgentPrefix != null) {
+            userAgent = userAgentPrefix + " " + userAgent;
+        }
+        if (userAgentPostfix != null) {
+            userAgent = userAgent + " " + userAgentPostfix;
+        }
+
+        // Set user agent
+        settings.setUserAgentString(userAgent);
+    }
 }
