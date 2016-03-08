b76a032d78d7610a1cc65fb7b407433ba8419358
Я <mnillcoway@gmail.com>
2016-03-08 19:14:33 +0300

    Implement android part.

--- src/android/UserAgent.java
+++ src/android/UserAgent.java
@@ -5,32 +5,40 @@ import org.apache.cordova.*;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class UserAgent extends CordovaPlugin {
+
     private static final String kUserAgent = "useragent";
-    private static final String kUserAgentPrefix = "useragentprefix";
-    private static final String kUserAgentPostfix = "useragentpostfix";
+    private WebSettings settings;
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         // Load cordova settings
         String userAgent = cordova.getActivity().getIntent().getStringExtra(kUserAgent);
-        String userAgentPrefix = cordova.getActivity().getIntent().getStringExtra(kUserAgentPrefix);
-        String userAgentPostfix = cordova.getActivity().getIntent().getStringExtra(kUserAgentPostfix);
 
         // Build user agent
-        WebSettings settings = ((WebView) webView.getEngine().getView()).getSettings();
+        settings = ((WebView) webView.getEngine().getView()).getSettings();
         if (userAgent == null) {
             userAgent = settings.getUserAgentString();
-        }
-        if (userAgentPrefix != null) {
-            userAgent = userAgentPrefix + " " + userAgent;
-        }
-        if (userAgentPostfix != null) {
-            userAgent = userAgent + " " + userAgentPostfix;
         }
 
         // Set user agent
         settings.setUserAgentString(userAgent);
+    }
+
+    @Override
+    public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        if (action.equals("setUserAgent")) {
+            final String userAgent = args.getString(0);
+            settings.setUserAgentString(userAgent);
+            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, new JSONArray()));
+        } else {
+            return false;
+        }
+        return true;
     }
 }
